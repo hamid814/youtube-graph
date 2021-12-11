eaf1b1d6035d61594b3bddb78c6836dbe227ca27
hamid814 <hamid331994@gmail.com>
2021-12-11 11:11:07 +0330

    dev

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ require('colors');
 
 const app = express();
 
+app.use(express.static('./client/build'));
+
 // log all incomming requests
 app.use((req, res, next) => {
   console.log(req.method.blue, req.path.green, req.params);
@@ -18,11 +20,11 @@ app.get('/api/data', (req, res) => {
   res.status(200).json(data);
 });
 
-if (process.env.NODE_ENV === 'production') {
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
+// if (process.env.NODE_ENV === 'production') {
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
+// }
 
 const PORT = process.env.PORT || 5000;
 
